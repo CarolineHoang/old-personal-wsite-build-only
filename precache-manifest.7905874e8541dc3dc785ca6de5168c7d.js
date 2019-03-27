b7a202928d553db6303773597676bf8f8754ef05
Cremebrutale <caroline.hoang@columbiaspectator.com>
2019-03-27 01:24:16 -0400

    please fix it

---
+++ precache-manifest.7905874e8541dc3dc785ca6de5168c7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2752e9d33f1da2c1aa",
+    "url": "/static/css/main.2a0bcf21.chunk.css"
+  },
+  {
+    "revision": "ab2752e9d33f1da2c1aa",
+    "url": "/static/js/main.f736a673.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3f26a2e32efd91d5ca73",
+    "url": "/static/js/2.a4e87c20.chunk.js"
+  },
+  {
+    "revision": "60fd142197aa484e7c2b21b7745869b0",
+    "url": "/static/media/matthew-brindle-1118459-unsplash.60fd1421.jpg"
+  },
+  {
+    "revision": "cca1ba6b34606bc04e78c827723e50b3",
+    "url": "/static/media/me.cca1ba6b.jpg"
+  },
+  {
+    "revision": "28ebae469a61393d2839106a4e247a8a",
+    "url": "/static/media/beautiful-blur-bright-326055.28ebae46.jpg"
+  },
+  {
+    "revision": "cc68a62cf6ee40bce80ae46e62d36b30",
+    "url": "/static/media/orlova-maria-1351165-unsplash.cc68a62c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4de3fe1cd22a72a4d896b0f8972396d9",
+    "url": "/index.html"
+  }
+];
